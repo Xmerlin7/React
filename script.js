2f4e29c70e621aa6205c9cd23b6b7cb1c3d5b158
Seif Allah <saifeldarageely@gmail.com>
2024-10-04 00:29:22 +0300

    Refactor code to render additional tasks in React component

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1> Hello, React!</h1>, document.getElementById("root"))
+ReactDOM.render(
+    <div>
+        <h1> Hello, React!</h1>
+        <ul>
+            <li>
+                Task 1
+            </li>
+            <li>
+                Task 2
+            </li>
+            <li>
+                Task 3
+            </li>
+        </ul>
+    </div>,
+    document.getElementById('root')
+)
