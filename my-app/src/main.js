4b7e5910c3aeece403079b663743be316696920a
Seif Allah <saifeldarageely@gmail.com>
2024-10-05 17:29:17 +0300

    Refactor App.js to add link to Seif Merlin in header

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,9 +1,20 @@
 export default function MyReact(){
+    const obj = {
+        name: "Seif",
+        aka: "merlion"
+    }                                            
     return (
         <div>
-            <h1>
-                Hello, reactjs
+            <h1 style={elmstlye}>
+                Hello, {obj.aka}
             </h1>
+            <button onClick={sayAlert}> Click</button>
         </div>
     )
+}
+const elmstlye = {
+    backgroundColor: "lightBlue"
+}   
+export function sayAlert(){
+    alert('Hi, Merlion')
 }
