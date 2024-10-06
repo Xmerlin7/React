6bb2eba2df9733634fde87741d01ac4dbd8d7e5a
Seif Allah <saifeldarageely@gmail.com>
2024-10-06 20:29:12 +0300

    Refactor App.js to add link to Seif Merlin in header and update styling

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -4,20 +4,23 @@ import Post from './posts';
 import SideBar from './sideBar'
 
 function App() {
+  const namee = "Seif";
   return (
     <div className="App">
       <Header />
       {/* Big container */}
-      <div style={ {display: "flex", justifyContent:'center', alignItems: 'center' ,marginTop: "120px" }}>
+      <div style={{ display: "flex", justifyContent: 'center', alignItems: 'center', marginTop: "120px" }}>
         {/* container that has two divs */}
-        <div style={{ display: "flex",gap: "50px", alignItems: "flex-start" }}>
+        <div style={{ display: "flex", gap: "50px", alignItems: "flex-start" }}>
           {/* posts div */}
-          <div  style={{width: "60%"}}>
-            <Post />
-            <Post />
+          <div style={{ width: "60%" }}>
+            <Post name="Merlin" />
+            <Post name="Adam">
+              <h3 style={{ color: namee === "Adam" ? "green" : "red" }}>Hi Hi captain</h3>
+            </Post>
           </div>
           {/* menu sidebar div */}
-          <div style={{width: "40%"}}>
+          <div style={{ width: "40%" }}>
             <SideBar />
           </div>
         </div>
--- first-project/src/posts.js
+++ first-project/src/posts.js
@@ -1,12 +1,14 @@
 import './posts.css'
-export default function Post() {
+export default function Post({name, children}) {
+
     return (
         <div className="posts">
             <h2>
-                This is the post title
+                This is the post is written by <span style={{color: "red"}}>{name}</span>
             </h2>
             <hr />
             <h1> This is the post content</h1>
+            {children}
         </div>
     );
 }
