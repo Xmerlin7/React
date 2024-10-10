6f21ceacb5d29ddc2eb81a8168bbc9f5ef451033
Seif Allah <saifeldarageely@gmail.com>
2024-10-10 18:09:30 +0300

    Refactor MyForm component to use useState hook for input values

--- first-project/src/myForm.js
+++ first-project/src/myForm.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 export default function MyForm() {
+  const [input, setInput] = useState({ name: "", email: "" });
   return (
     <form>
-      <label style={{marginRight: "5px"}}>Name:</label>
-      <input type="text" placeholder="username"></input>
+      <label style={{ marginRight: "5px" }}>Name:</label>
+      <input
+        value={input.name}
+        onChange={(e) => {
+          setInput({ name: e.target.value });
+        }}
+        type="text"
+        placeholder="username"
+      ></input>
       <hr />
-      <label style={{marginRight: "5px"}}>Email:</label>
-      <input type="text" placeholder="email"></input>
+      <label style={{ marginRight: "5px" }}>Email:</label>
+      <input
+        value={input.email}
+        onChange={(e) => {
+          setInput({ email: e.target.value });
+        }}
+        type="text"
+        placeholder="email"
+      ></input>
       <hr />
-      <button >Click</button>
+      <button>Click</button>
     </form>
   );
 }
